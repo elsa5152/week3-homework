b2036fd672ae0f939f1432ea76839fb2d5438536
Elsa <ljq5152@gmail.com>
2022-03-26 16:13:22 +1030

    write js code

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # week3-homework
+
+This is the detail for week3-homework.
+
+* Create a new repository on my Github account and creat a README.md file.
+<img src="image/week3repo.png" alt="week3repo.png" width="400" height="200">
+* Clone all the file form Gitlab to my Github account.
+* Write Javascript code in vs Code.
+<img src="assets/image/workingFile.png" alt="workingFile_image" width="400" height="400">
+* Write the code in vs Code and upload it by gitpush.
+<img src="./assets/image/upload-code.png" alt="upload-code.png" width="400" height="200">
+
+* Update to the repo
+* Create a webpage link
+
+## Link my homework
+
+[Lernantino]( https://elsa5152.github.io/week2-homework/)
--- script.js
+++ script.js
@@ -10,5 +10,41 @@ function writePassword() {
 
 }
 
+function generatePassword () {
+  // prompt for the length of the password
+  var passLength = prompt("Please enter how many characters you would like your password to be. \nMust be between 8 - 128 characters.");
+  
+  var lengthConfirm = parseInt(passLength);
+  console.log(lengthConfirm);
+
+// choose a length of at least 8 characters and no more than 128 characters
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+      alert ("Error. Choice is not valid. Please try again.");
+      return;
+  }
+
+var collectionOfValids = [];
+var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("");
+var lowerCaseChar = "abcdefghiklmnopqrsttuvwxtz" .split("");
+var numberChar = "0123456789" .split ("");
+var specialChar = "*&^%$#@!?><{}" .split("");
+
+// we need a confirm to ask the user if they want to use lowercase
+var lowerCaseConfirm = confirm("Do you want your password to include lowercase letters?");
+    if(lowerCaseConfirm === true) {
+        for (var i = 0; i < lowerCaseChar.length; i++) {
+            collectionOfValids.push(lowerCaseChar[i]);
+        }
+    }
+// we need a confirm to ask the user if they want to use uppercase
+var upperCaseConfirm = confirm("Do you want your password to include uppercase letters?");
+    if (upperCaseConfirm === true) {
+        for (var i = 0; i < upperCaseChar.length; i++) {
+            collectionOfValids.push(upperCaseChar[i]);
+        }
+    }
+// we need a confirm to ask the user if they want to use numeric
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
